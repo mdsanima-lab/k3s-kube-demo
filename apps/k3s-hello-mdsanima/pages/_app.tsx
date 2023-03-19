ed3977b78a0923e03ffb5f7a53ef48a2a1b944f0
mdsanima <marcinrozewski@gmail.com>
2023-03-19 01:39:34 +0100

    feat: add rgb definition mdsanima colors palette

--- apps/k3s-hello-mdsanima/pages/_app.tsx
+++ apps/k3s-hello-mdsanima/pages/_app.tsx
@@ -1,7 +1,8 @@
-/** Copyright © 2023 Marcin Różewski MDSANIMA */
+/* Copyright © 2023 Marcin Różewski MDSANIMA */
 
 import type { AppProps } from "next/app"
 
+import "@mdsanima-lab/styles/colors.css"
 import "@mdsanima-lab/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
